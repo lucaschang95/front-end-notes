3f8f2e342e598fea0615994a24b10a229be6938b
Xingzhao Chang <xingzhao.chang@gmail.com>
2020-10-09 22:57:35 +0800

    sandbox

---
+++ design-pattern/sandbox.js
@@ -0,0 +1,59 @@
+function Sandbox() {
+    let args = Array.prototype.slice.call(arguments),
+        callback = args.pop(),
+        modules = (args[0] && typeof args[0] === 'string') ? args : args[0],
+        i;
+        if (!(this instanceof Sandbox)) {
+            return new Sandbox(modules, callback);
+        }
+
+        
+
+        this.a = 1;
+        this.b = 2;
+
+        if (!modules || modules === '*') {
+            modules = [];
+            for(i in Sandbox.modules) {
+                if (Sandbox.modules.hasOwnProperty(i)) {
+                    modules.push(i);
+                }
+            }
+
+            for(i = 0; i < modules.length; i++) {
+                Sandbox.modules[modules[i]](this);
+            }
+
+            callback(this);
+        }
+};
+
+Sandbox.prototype = {
+    name: 'my application',
+    version: '1.0',
+    getName: function() {
+        return this.name;
+    },
+};
+
+Sandbox.modules = {};
+
+Sandbox.modules.dom = function(box) {
+    box.getElement = function() {};
+    box.getStyle = function() {};
+    box.foo = 'bar';
+};
+
+Sandbox.modules.event = function(box) {
+    box.attachEvent = function() {};
+    box.dettachEvent = function() {};
+};
+
+Sandbox.modules.ajax = function(box) {
+    box.makeRequest = function() {};
+    box.getResponse = function() {};
+};
+
+Sandbox('a', function() {
+
+});
